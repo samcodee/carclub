101c13b1165a7e97699d9dd067c198f7f5f8f5d5
samcodee <62462363+samcodee@users.noreply.github.com>
2020-04-23 20:32:00 +0530

    Added some more logs

--- js/wikiapi.js
+++ js/wikiapi.js
@@ -1,4 +1,5 @@
 async function parseWikiPage(pageTitle) {
+     console.log("entered async function.....");
      const url = "https://en.wikipedia.org/w/api.php?" +
         new URLSearchParams({
             origin: "*",
@@ -8,10 +9,12 @@ async function parseWikiPage(pageTitle) {
         });
 
     try {
+        console.log("about to make api call"); 
         const req = await fetch(url);
         const json = await req.json();
         console.log(json.parse.text["*"]);
         parsedText = json.parse.text["*"];
+        console.log("finished api call..... Returning contents....");
         //document.getElementById("wikiContents").innerHTML = parsedText;     
         return parsedText;
     } catch (e) {
