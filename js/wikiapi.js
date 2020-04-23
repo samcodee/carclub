53e41482331470b188bb7e94088121127ba06499
samcodee <62462363+samcodee@users.noreply.github.com>
2020-04-23 19:53:09 +0530

    Created Wiki API for querying the contents

---
+++ js/wikiapi.js
@@ -0,0 +1,19 @@
+function parseWikiPage(pageTitle) {
+     const url = "https://en.wikipedia.org/w/api.php?" +
+        new URLSearchParams({
+            origin: "*",
+            action: "parse",
+            page: pageTitle,
+            format: "json",
+        });
+
+    try {
+        const req = await fetch(url);
+        const json = await req.json();
+        console.log(json.parse.text["*"]));
+        parsedText = json.parse.text[*];
+        return parsedText;
+    } catch (e) {
+        console.error(e);
+    }   
+}
